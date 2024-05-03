57408ab7fe400687e8b438dc090b39b95946e777
dagmawibabi <1babidagi@gmail.com>
2024-05-03 18:38:43 +0300

    api done

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+//! DATABASE
+import { MongoClient } from 'mongodb'
+
+//! Connection URL
+const url = process.env.MONGODB_URL || "";
+const client = new MongoClient(url);
+const dbName = 'communities';
+
+//! CONNECT TO DB
+async function main() {
+  await client.connect();
+  console.log('Connected successfully to server');
+}
+
+//* ROUTES
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+//* GET ALL COMMUNITIES
+app.get('/getCommunities', async(c) => {
+  const db = client.db(dbName);
+  const collection = db.collection('communities');
+
+  let foundCommunities = await collection.find({}).toArray()
+  return c.json(foundCommunities)
+})
+
+//* SUBMIT A NEW COMMUNITY
+app.post('/submitCommunity', async (c) => {
+  const body = await c.req.parseBody();
+
+  const db = client.db(dbName);
+  const collection = db.collection('communities');
+
+  let newCommunity = await collection.insertOne(body)
+  let foundCommunities = await collection.find({}).toArray()
+
+  return c.json(foundCommunities)
+})
+
+//* ERROR HANDLING
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.json(err.message)
+})
+
+
+//! INIT DB
+main()
+
+export default app
